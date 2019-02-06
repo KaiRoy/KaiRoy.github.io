bbe5ac40e2a8976b9a2904776b2b3bc32394efc9
Kai Roy <kai7lego+github@gmail.com>
2019-02-06 14:02:51 -0800

    Update button.js

--- button.js
+++ button.js
@@ -1,13 +1,11 @@
 //Project 1 Button
 var counter1 = 0;
-var counter2 = 0;
-
 var contentsm1 = "More Content";
 var contentsl1 = "Less Content";
 document.getElementById("button1").addEventListener("click", ContentChange("button1", counter1, "data1", contentsm1, contentsl1));
 
 //Project 2 Button
-
+var counter2 = 0;
 var contentsm2 = "Even More Content";
 var contentsl2 = "Even Less Content";
 document.getElementById("button2").addEventListener("click", ContentChange("button2", counter2, "data2", contentsm2, contentsl2));
