dad8e906939f5b420a6a8e89fe93648259c5147b
Kai Roy <kai7lego+github@gmail.com>
2019-02-06 13:55:50 -0800

    Add button.js via upload

---
+++ button.js
@@ -0,0 +1,25 @@
+//Project 1 Button
+var counter1 = 0;
+var contentsm1 = "More Content";
+var contentsl1 = "Less Content";
+document.getElementById("button1").addEventListener("click", ContentChange("button1", counter1, "data1", contentsm1, contentsl1));
+
+//Project 2 Button
+var counter2 = 0;
+var contentsm2 = "More Content";
+var contentsl2 = "Less Content";
+document.getElementById("button2").addEventListener("click", ContentChange("button2", counter2, "data2", contentsm2, contentsl2));
+
+//Show moreless function
+function ContentChange(btn, counter, data, contentsm, contentsl) {
+	if (counter == 0) { 
+		document.getElementById(data).innerHTML = contentsl;
+		document.getElementById(btn).innerHTML = "Show More";
+		counter = 1;
+	}
+	else { 
+		document.getElementById(data).innerHTML = contentsm;    
+		document.getElementById(btn).innerHTML = "Show Less";
+		counter = 0;
+	}
+}
