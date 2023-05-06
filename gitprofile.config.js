c41abccddb9d4fd53116c296866b6bc95c45daca
Kai Roy <kai7lego+github@gmail.com>
2023-05-06 10:58:44 -0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/uc?export=download&id=1LOdvfY2EBZEqmx96hXaZYTXlIO3m513wfT7tbKZHePg', // Empty fileUrl will hide the `Download Resume` button.
+      'https://docs.google.com/document/d/1LOdvfY2EBZEqmx96hXaZYTXlIO3m513wfT7tbKZHePg/edit?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [	//Needs to be updated
     'KiCad',	
